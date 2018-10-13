78994c06f9cd221dcef10e42c86c677084225d9a
xhuang8 <xhuang8@mail.sfsu.edu>
2018-10-12 18:06:04 -0700

    User can drag card using a pan gesture

---
+++ Tinder/CardsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CardsViewController.swift
+//  Tinder
+//
+//  Created by XiaoQian Huang on 10/12/18.
+//  Copyright © 2018 XiaoQian Huang. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    var cardInitialCenter: CGPoint!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
